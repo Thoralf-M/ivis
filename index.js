131602249136f07437d8514e0f76f1f44a7572a2
iota-cm <me@john.fitness>
2019-01-23 00:39:00 +1100

    resolve bugs, still an issue with delete queue

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const sock = zmq.socket('sub');
 sock.connect('tcp://trinity.iota-tangle.io:5556');
 sock.subscribe('tx_trytes');
 sock.subscribe('sn');
-sock.subscribe('ms');
+sock.subscribe('lmhs');
 
 app.use("/", express.static(__dirname));
 
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 
-var txCount = 0;
+const initLimit = 2000;
 var initTx = [];
 var initSn = [];
 var initMs = [];
@@ -29,34 +29,35 @@ sock.on('message', msg => {
   switch (data[0]) {
     case 'sn':
         initSn.push({ hash: data[1]})
-        if (initSn.length > 1000) {
+        if (initSn.length > initLimit) {
           initSn.shift();
         }
         console.log("sn: " + initSn.length);  
+      break
+    case 'lmhs':
+      initMs.push({ hash: data[1]})
+      if (initMs.length > initLimit) {
+        initMs.shift();
+      }
+      console.log("ms: " + initMs.length);  
       break
     default:              
       const tx = iotaUtils.transactionObject(data[1], data[0]);    
       var txObj = { 
         hash: tx.hash, 
-        transaction_trunk: tx.branchTransaction,  
+        transaction_trunk: tx.trunkTransaction,  
         transaction_branch: tx.branchTransaction,
         bundle_hash: tx.bundle,
+        current_index: tx.currentIndex,
+        last_index: tx.lastIndex,
         tag: tx.tag,
         value: tx.value
       };
-      if (data[0] === "tx_trytes") {
-        initTx.push(txObj)
-        if (initTx.length > 1000) {
-          initTx.shift();
-        }
-        console.log("tx: " + initTx.length);  
-      } else {
-        initMs.push(txObj)
-        if (initMs.length > 1000) {
-          initMs.shift();
-        }
-        console.log("ms: " + initMs.length);  
+      initTx.push(txObj)
+      if (initTx.length > initLimit) {
+        initTx.shift();
       }
+      console.log("tx: " + initTx.length);       
       break
     }    
 });
@@ -72,21 +73,22 @@ io.on('connection', function(socket){
       case 'sn':
         socket.emit('sn', data[1]);
         break
+      case 'lmhs':
+        socket.emit('ms', data[1]);
+        break
       default:                
         const tx = iotaUtils.transactionObject(data[1], data[0]);    
         var txObj = { 
           hash: tx.hash, 
-          transaction_trunk: tx.branchTransaction,  
+          transaction_trunk: tx.trunkTransaction,  
           transaction_branch: tx.branchTransaction,
           bundle_hash: tx.bundle,
+          current_index: tx.currentIndex,
+          last_index: tx.lastIndex,
           tag: tx.tag,
           value: tx.value
         };
-        if (data[0] === "tx_trytes") {               
-          socket.emit('tx', txObj);
-        } else {
-          socket.emit('ms', txObj);
-        }
+        socket.emit('tx', txObj);        
         break
       }  
     //console.log(tx.hash);        
