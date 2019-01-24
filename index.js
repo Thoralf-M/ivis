88a47632a36b9ceba24d310ce2e161c59057a867
Rajiv Shah <rajivshah1@icloud.com>
2019-01-23 19:26:13 -0500

    Resolve lint errors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const iotaUtils = require('iota.lib.js/lib/utils/utils');
 const zmq = require('zeromq');
+
 const sock = zmq.socket('sub');
 
 sock.connect('tcp://trinity.iota-tangle.io:5556');
@@ -11,90 +12,90 @@ sock.subscribe('tx_trytes');
 sock.subscribe('sn');
 sock.subscribe('lmhs');
 
-app.use("/", express.static(__dirname));
+app.use('/', express.static(__dirname));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-})
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
 
 const initLimit = 2000;
-var initTx = [];
-var initSn = [];
-var initMs = [];
+const initTx = [];
+const initSn = [];
+const initMs = [];
 
-sock.on('message', msg => {  
-  //console.log(txCount);    
+sock.on('message', (msg) => {
+  // console.log(txCount);
   const data = msg.toString().split(' ');
-  //console.log(data[2] + " " + data[1]);    
+  // console.log(data[2] + " " + data[1]);
   switch (data[0]) {
     case 'sn':
-        initSn.push({ hash: data[1]})
-        if (initSn.length > initLimit) {
-          initSn.shift();
-        }
-        console.log("sn: " + initSn.length);  
-      break
+      initSn.push({ hash: data[1] });
+      if (initSn.length > initLimit) {
+        initSn.shift();
+      }
+      console.log(`sn: ${initSn.length}`);
+      break;
     case 'lmhs':
-      initMs.push(data[1])
+      initMs.push(data[1]);
       if (initMs.length > initLimit) {
         initMs.shift();
       }
-      console.log("ms: " + initMs.length);  
-      break
-    default:              
-      const tx = iotaUtils.transactionObject(data[1], data[0]);    
-      var txObj = { 
-        hash: tx.hash, 
-        transaction_trunk: tx.trunkTransaction,  
+      console.log(`ms: ${initMs.length}`);
+      break;
+    default:
+      const tx = iotaUtils.transactionObject(data[1], data[0]);
+      const txObj = {
+        hash: tx.hash,
+        transaction_trunk: tx.trunkTransaction,
         transaction_branch: tx.branchTransaction,
         bundle_hash: tx.bundle,
         current_index: tx.currentIndex,
         last_index: tx.lastIndex,
         tag: tx.tag,
-        value: tx.value
+        value: tx.value,
       };
-      initTx.push(txObj)
+      initTx.push(txObj);
       if (initTx.length > initLimit) {
         initTx.shift();
       }
-      console.log("tx: " + initTx.length);       
-      break
-    }    
+      console.log(`tx: ${initTx.length}`);
+      break;
+  }
 });
 
 
-io.on('connection', function(socket){    
+io.on('connection', (socket) => {
   socket.emit('inittx', initTx);
   socket.emit('initsn', initSn);
-  socket.emit('initms', initMs);  
-  sock.on('message', msg => {        
+  socket.emit('initms', initMs);
+  sock.on('message', (msg) => {
     const data = msg.toString().split(' ');
     switch (data[0]) {
       case 'sn':
         socket.emit('sn', { hash: data[1] });
-        break
+        break;
       case 'lmhs':
         socket.emit('ms', data[1]);
-        break
-      default:                
-        const tx = iotaUtils.transactionObject(data[1], data[0]);    
-        var txObj = { 
-          hash: tx.hash, 
-          transaction_trunk: tx.trunkTransaction,  
+        break;
+      default:
+        const tx = iotaUtils.transactionObject(data[1], data[0]);
+        const txObj = {
+          hash: tx.hash,
+          transaction_trunk: tx.trunkTransaction,
           transaction_branch: tx.branchTransaction,
           bundle_hash: tx.bundle,
           current_index: tx.currentIndex,
           last_index: tx.lastIndex,
           tag: tx.tag,
-          value: tx.value
+          value: tx.value,
         };
-        socket.emit('tx', txObj);        
-        break
-      }  
-    //console.log(tx.hash);        
+        socket.emit('tx', txObj);
+        break;
+    }
+    // console.log(tx.hash);
   });
 });
 
-server.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+server.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
